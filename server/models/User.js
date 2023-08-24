ee8546f71b6662ccc995381cf2f99a766c579427
Maxim Rogozin <maximrogozin@icloud.com>
2023-08-25 00:10:34 +0500

    prod v0.4.1 update api

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,8 @@ const schema = new Schema(
     name: { type: String },
     email: { type: String, require: true, unique: true },
     password: { type: String },
+    rootAdmin: { type: Boolean },
+    rootManager: { type: Boolean },
   },
   {
     timestamps: true,
